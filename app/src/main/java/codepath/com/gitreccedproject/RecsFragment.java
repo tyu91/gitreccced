1926bcd75b1601c57c058e87650f64f6ee3a7e16
X Norman <xnorman@fb.com>
2018-07-23 12:17:55 -0700

    Infinite Pagination

--- app/src/main/java/codepath/com/gitreccedproject/RecsFragment.java
+++ app/src/main/java/codepath/com/gitreccedproject/RecsFragment.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 
 public class RecsFragment extends Fragment {
+    // Store a member variable for the listener
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     public static Fragment getInstance(int position) {
         Bundle bundle = new Bundle();
@@ -43,7 +45,7 @@ public class RecsFragment extends Fragment {
         rv_tvShows = view.findViewById(R.id.rv_tv);
         rv_books = view.findViewById(R.id.rv_books);
 
-        final LinearLayoutManager movies = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager movies = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         LinearLayoutManager tvShows = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         LinearLayoutManager books = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
 
@@ -56,10 +58,32 @@ public class RecsFragment extends Fragment {
         rv_tvShows.setAdapter(recAdapter);
         rv_books.setAdapter(recAdapter);
 
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessRecyclerViewScrollListener(movies) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(page);
+            }
+        };
+
+        rv_movies.addOnScrollListener(scrollListener);
+
         //TODO - change this to get actual data
         for (int i = 0; i < 5; i++) {
             Item item = null;items.add(item);
             recAdapter.notifyItemInserted(items.size() - 1);
         }
+    }
+
+    // Append the next page of data into the adapter
+    // This method probably sends out a network request and appends new data items to your adapter.
+    public void loadNextDataFromApi(int offset) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
     }
 }
