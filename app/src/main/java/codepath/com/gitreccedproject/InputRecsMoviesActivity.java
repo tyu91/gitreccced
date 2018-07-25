7567e411969d1a752591864b72db60617417bbff
X Norman <xnorman@fb.com>
2018-07-25 11:21:59 -0700

    Minor Changes

--- app/src/main/java/codepath/com/gitreccedproject/InputRecsMoviesActivity.java
+++ app/src/main/java/codepath/com/gitreccedproject/InputRecsMoviesActivity.java
@@ -1,7 +1,9 @@
 package codepath.com.gitreccedproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +11,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.algolia.search.saas.AlgoliaException;
