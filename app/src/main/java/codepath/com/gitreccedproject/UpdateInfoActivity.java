b69fd54b1f11df918779487711835be5d0a85b39
Tomoka Kan <tomokak@fb.com>
2018-08-09 12:19:25 -0700

    settings

--- app/src/main/java/codepath/com/gitreccedproject/UpdateInfoActivity.java
+++ app/src/main/java/codepath/com/gitreccedproject/UpdateInfoActivity.java
@@ -1,5 +1,6 @@
 package codepath.com.gitreccedproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.parceler.Parcels;
 
 public class UpdateInfoActivity extends AppCompatActivity {
 
@@ -17,8 +20,10 @@ public class UpdateInfoActivity extends AppCompatActivity {
     Button btnUpdateUsername;
     Button btnUpdateEmail;
     Button btnUpdatePassword;
+    Button btn_done;
 
     UpdateInfoDialog updateInfoDialog;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,10 @@ public class UpdateInfoActivity extends AppCompatActivity {
         btnUpdateUsername = findViewById(R.id.btnUpdateUsername);
         btnUpdateEmail = findViewById(R.id.btnUpdateEmail);
         btnUpdatePassword = findViewById(R.id.btnUpdatePassword);
+        btn_done = findViewById(R.id.btn_done);
 
         //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        User user = LoginActivity.currentuser;
+        user = LoginActivity.currentuser;
 
         tvUsername.setText(user.getUsername());
         tvEmail.setText(user.getEmail());
@@ -78,5 +84,23 @@ public class UpdateInfoActivity extends AppCompatActivity {
                 updateInfoDialog.show(manager, "This is a test");
             }
         });
+
+        btn_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UpdateInfoActivity.this, RecsFragment.class);
+                i.putExtra("user", Parcels.wrap(user));
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent i = new Intent(UpdateInfoActivity.this, RecsFragment.class);
+        i.putExtra("user", Parcels.wrap(user));
+        startActivity(i);
+        finish();
     }
 }
