1fc2c1e200daccb8dfdfd06dd31550787149166a
tyu91 <tiffanyy9968@gmail.com>
2018-08-10 12:09:19 -0700

    added fields to details page

--- app/src/main/java/codepath/com/gitreccedproject/LibraryFragment.java
+++ app/src/main/java/codepath/com/gitreccedproject/LibraryFragment.java
@@ -144,6 +144,7 @@ public class LibraryFragment extends Fragment {
                         item.setMovieId(postSnapshot.child("movieId").getValue().toString());
                         item.setPosterPath(postSnapshot.child("posterPath").getValue().toString());
                         item.setBackdropPath(postSnapshot.child("backdropPath").getValue().toString());
+                        item.setReleaseDate(postSnapshot.child("releaseDate").getValue().toString());
                         //item.setDetails(postSnapshot.child("overview").getValue().toString());
                         movieslib.add(item);
 
@@ -158,6 +159,8 @@ public class LibraryFragment extends Fragment {
                         item.setBookId(postSnapshot.child("bookId").getValue().toString());
                         item.setSmallImgUrl(postSnapshot.child("smallImgUrl").getValue().toString());
                         item.setImgUrl(postSnapshot.child("imgUrl").getValue().toString());
+                        item.setAuthor(postSnapshot.child("author").getValue().toString());
+                        item.setPubYear(postSnapshot.child("pubYear").getValue().toString());
                         booklib.add(item);
                     }
                 }
--- app/src/main/java/codepath/com/gitreccedproject/MovieDetailsFragment.java
+++ app/src/main/java/codepath/com/gitreccedproject/MovieDetailsFragment.java
@@ -28,15 +28,6 @@ public class MovieDetailsFragment extends Fragment {
     public MovieDetailsFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MovieDetailsFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static MovieDetailsFragment newInstance(String param1, String param2) {
         MovieDetailsFragment fragment = new MovieDetailsFragment();
@@ -106,43 +97,4 @@ public class MovieDetailsFragment extends Fragment {
 //            }
 //        });
     }
-
-    /*// TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }*/
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*public interface OnFragmentInteractionListener {
-         //TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
